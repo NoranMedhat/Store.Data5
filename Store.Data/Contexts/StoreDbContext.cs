1e6454576ba39d8f12f73639d57db33600b994e2
Noran <Noranmedht63@gmail.com>
2024-10-09 13:32:08 +0300

    Add project files.

---
+++ Store.Data/Contexts/StoreDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Data.Contexts
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<DeliveryMethod> DeliveryMethod { get; set; }
+
+    }
+}
---
+++ Store.Repository/Basket/Models/CustomerBasket.cs
@@ -0,0 +1,11 @@
+﻿namespace Store.Repository.Basket.Models
+{
+    public class CustomerBasket
+    {
+        public string? Id { get; set; }
+        public int? DeliveryMethodId { get; set; }
+        public decimal ShippingPrice { get; set; }
+        public List<BasketItem> BasketItems { get; set; } = new List<BasketItem>();
+
+    }
+}
